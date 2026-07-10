23d7ee5176300f1ba243b4bf02669cf25d1f9bec
rajanraj5431-jpg <rajanraj5431@gmail.com>
2026-07-10 15:13:37 +0530

    Time: 1 ms (29.79%) | Memory: 9.6 MB (17.61%) - LeetSync

---
+++ 394-decode-string/decode-string.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    string decodeString(string s) {
+        stack<int> countStack;
+        stack<string> stringStack;
+
+        string curr = "";
+        int num = 0;
+
+        for (char c : s) {
+            if (isdigit(c)) {
+                num = num * 10 + (c - '0');
+            }
+            else if (c == '[') {
+                countStack.push(num);
+                stringStack.push(curr);
+
+                num = 0;
+                curr = "";
+            }
+            else if (c == ']') {
+                int k = countStack.top();
+                countStack.pop();
+
+                string temp = stringStack.top();
+                stringStack.pop();
+
+                while (k--) {
+                    temp += curr;
+                }
+
+                curr = temp;
+            }
+            else {
+                curr += c;
+            }
+        }
+
+        return curr;
+    }
+};
