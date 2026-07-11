8d69f6fba18a064a0bfa541d0a5ed055616cfcab
rajanraj5431-jpg <rajanraj5431@gmail.com>
2026-07-12 00:16:26 +0530

    Time: 0 ms (100.00%) | Memory: 22.3 MB (9.76%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -15,9 +15,6 @@ public:
         if(root==NULL){
             return 0;
         }
-        if(root->left==NULL&&root->right==NULL){
-            return 1;
-        }
         return max(maxDepth(root->left), maxDepth(root->right))+1;
     }
 };
