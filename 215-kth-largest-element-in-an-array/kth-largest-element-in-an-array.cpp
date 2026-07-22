f13ef68060683a9cc53833e4ab9e4b609ca2aac0
rajanraj5431-jpg <rajanraj5431@gmail.com>
2026-07-22 22:48:49 +0530

    Time: 2166 ms (5.00%) | Memory: 68.9 MB (76.89%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,9 +1,38 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        // nth_element puts the k-th largest element in its sorted position
-        // Since we want k-th largest, we look at index n-k from the beginning
-        nth_element(nums.begin(), nums.begin() + nums.size() - k, nums.end());
-        return nums[nums.size() - k];
+        // We want the k-th largest, which is the (n-k)-th smallest (0-indexed)
+        int target = nums.size() - k;
+        int left = 0, right = nums.size() - 1;
+        
+        while (left < right) {
+            int pivotIndex = partition(nums, left, right);
+            if (pivotIndex == target) {
+                return nums[pivotIndex];
+            } else if (pivotIndex < target) {
+                left = pivotIndex + 1;
+            } else {
+                right = pivotIndex - 1;
+            }
+        }
+        return nums[left];
+    }
+
+private:
+    int partition(vector<int>& nums, int left, int right) {
+        // Random pivot to avoid worst-case O(n^2) on sorted inputs
+        int pivotIndex = left + rand() % (right - left + 1);
+        swap(nums[pivotIndex], nums[right]);
+        int pivot = nums[right];
+        
+        int storeIndex = left;
+        for (int i = left; i < right; ++i) {
+            if (nums[i] < pivot) {
+                swap(nums[i], nums[storeIndex]);
+                storeIndex++;
+            }
+        }
+        swap(nums[storeIndex], nums[right]);
+        return storeIndex;
     }
 };   
