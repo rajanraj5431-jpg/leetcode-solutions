a203ec3ae945f62b5b3005cdc81ecc8e2961124a
rajanraj5431-jpg <rajanraj5431@gmail.com>
2026-08-03 12:54:16 +0530

    Time: 2 ms (19.26%) | Memory: 8.7 MB (62.12%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.cpp
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -1,31 +1,28 @@
 class Solution {
 public:
-    vector<vector<int>> combinationSum3(int k, int n) {
-        int s=k*(k+1)/2;
-        if(n<s){
-            return {};
+    vector<vector<int>> ans;
+    vector<int> path;
+
+    void dfs(int start, int k, int target) {
+
+        if(path.size() == k) {
+            if(target == 0)
+                ans.push_back(path);
+            return;
         }
-        const vector<int> num = {1,2,3,4,5,6,7,8,9};
-        vector<vector<int>> ans;
-        vector<int> path;
-        function<void(int)> backtrack = [&](int index){
-            if(path.size()==k){
-                int sum=0;
-                for(int i=0;i<k;i++){
-                    sum+=path[i];
-                }
-                if(sum==n){
-                    ans.push_back(path);
-                }
-                return;
-            }
-            for(int i=index;i<9;i++){
-                path.push_back(num[i]);
-                backtrack(i+1);
-                path.pop_back();
-            }
-        };
-        backtrack(0);
-        return ans;   
+
+        for(int i = start; i <= 9; i++) {
+
+            if(i > target) break;
+
+            path.push_back(i);
+            dfs(i + 1, k, target - i);
+            path.pop_back();
+        }
+    }
+
+    vector<vector<int>> combinationSum3(int k, int n) {
+        dfs(1, k, n);
+        return ans;
     }
 };
